1a3944ba411424b8edc789640cfda4e1bf9423de
Jaime Valencia <jaime.valencia@salesdecameron.com>
2022-11-26 14:45:52 -0500

    Construct method on PHP and change to server index

--- PHP/User.php
+++ PHP/User.php
@@ -1,12 +1,23 @@
 <?php
 class User{
     # Attributes
-    $Id;
-    $Document;
-    $Type;
-    $Name;
-    $Password;
-    $Email;
-    $Address;
+    public $Id;
+    public $Document;
+    public $Type;
+    public $Name;
+    public $Password;
+    public $Email;
+    public $Address;
+
+    #Methods
+    public function __construct($Name, $Document)
+    {
+        $this->Name = $Name;
+        $this->Document = $Document;
+    }
+
+    public function printDataUser()
+    {
+        echo "Name: $this->Name";
+    }
 }
-    
---
+++ PHP/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once ('User.php');
+
+$user = new User("Jaime Valencia", "1105792813");
+$user->printDataUser();
